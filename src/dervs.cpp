fdba86b277c933b97ef1c1488a986051e4914578
Roberto Di Remigio <roberto.diremigio@gmail.com>
2022-05-02 09:37:14 +0200

    Save experiments on autodiff of cavity grid weights

---
+++ src/dervs.cpp
@@ -0,0 +1,75 @@
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+#include <spdlog/spdlog.h>
+
+#include <autodiff/forward/dual.hpp>
+#include <autodiff/forward/dual/eigen.hpp>
+
+using namespace autodiff;
+
+template <typename T>
+auto generate(const Eigen::Vector3d &C, const Eigen::Vector<T, 3> &x) -> T {
+  return 1.0 / (C - x).norm();
+}
+
+template <typename T>
+auto generate2(const Eigen::Vector3d &C,
+               const Eigen::Matrix<T, 3, Eigen::Dynamic> &xs) -> T {
+  return 1.0 / (C - xs.rowwise().sum()).norm();
+}
+
+template <typename T>
+auto generate3(const Eigen::Vector3d &C,
+               const Eigen::Matrix<T, 3, Eigen::Dynamic> &xs)
+    -> Eigen::Vector<T, Eigen::Dynamic> {
+  std::vector<T> tmps;
+
+  for (decltype(xs.cols()) i = 0; i < xs.cols(); ++i) {
+    tmps.push_back(1.0 / (C - xs.col(i)).norm());
+    // ys(i) = 1.0 / (C - xs.col(i)).norm();
+  }
+
+  Eigen::Vector<T, Eigen::Dynamic> ys =
+      Eigen::Map<Eigen::Vector<T, Eigen::Dynamic>>(tmps.data(), tmps.size());
+
+  return ys;
+}
+// The scalar function for which the gradient is needed
+template <typename T>
+T foo(const Eigen::Vector<T, Eigen::Dynamic> &x) {
+
+  SPDLOG_INFO("x {}", x);
+  return x.dot(x);
+}
+
+int main() {
+  spdlog::set_pattern("[%Y-%m-%d %T][%^%l%$][TID: %t, PID: %P][%!@%s:%4#] %v");
+
+  using T = dual;
+
+  Eigen::Vector3d C(1.0, -1.0, M_PI);
+
+  Eigen::Matrix<T, 3, Eigen::Dynamic> xs{{0.0, 0.0}, {0.0, 0.0}, {0.0, M_PI}};
+
+  Eigen::Vector<T, 3> D(1.0, 0.0, -M_PI);
+
+  auto [f, dfdx] = derivatives(foo<T>, wrt(D), at(D));
+  SPDLOG_INFO("f {}", f);
+  SPDLOG_INFO("dfdx {}", dfdx);
+
+  //auto [g, dfdxs] = derivatives(generate3<T>, wrt(xs), at(C, xs));
+  //SPDLOG_INFO("dfdxs {}", dfdxs);
+  //Eigen::MatrixXd Z = jacobian(generate3<T>, wrt(xs), at(C, xs));
+
+  // SPDLOG_INFO("Z of dimension {}x{}", Z.rows(), Z.cols());
+  // SPDLOG_INFO("Z\n{}", Z);
+
+  return EXIT_SUCCESS;
+}
--- src/foo.cpp
+++ src/foo.cpp
@@ -1,6 +1,8 @@
 #include <array>
 #include <cmath>
 #include <cstdlib>
+#include <iterator>
+#include <map>
 #include <numeric>
 #include <vector>
 
@@ -11,8 +13,21 @@
 #include <fmt/ranges.h>
 #include <spdlog/spdlog.h>
 
+// autodiff include
+#include <autodiff/forward/real.hpp>
+#include <autodiff/forward/real/eigen.hpp>
+
+#include "LeopardiPartition.hpp"
 #include "Sphere.hpp"
 #include "TsLess.hpp"
+#include "utils/FiniteDifference.hpp"
+
+using namespace autodiff;
+
+// The scalar function for which the gradient is needed
+Vector3dual f(const Vector3dual &r_i, const Vector3dual &r_j) {
+  return (r_i - r_j) / pow((r_i - r_j).norm(), 3);
+}
 
 using namespace classyq;
 
@@ -59,6 +74,65 @@ Eigen::MatrixXd Dmatrix(const TsLess &cavity) {
   return D;
 }
 
+using Stencil = std::map<int, std::vector<double>>;
+
+// auto first = Stencil{
+//     {3, std::vector{-1.0 / 2.0, 0.0, 1.0 / 2.0}},
+//     {5, std::vector{1.0 / 12.0, -2.0 / 3.0, 0.0, 2.0 / 3.0, -1.0 / 12.0}},
+//     {9, std::vector{1.0 / 280.0, -4.0 / 105.0, 1.0 / 5.0, -4.0 / 5.0, 0.0,
+//                     4.0 / 5.0, -1.0 / 5.0, 4.0 / 105.0, -1.0 / 280.0}}};
+
+auto first = Stencil{
+    {5, std::vector{1.0 / 12.0, -2.0 / 3.0, 0.0, 2.0 / 3.0, -1.0 / 12.0}}};
+
+auto stencils = std::map<int, Stencil>{{1, first}};
+
+constexpr auto second_derivative = std::array<double, 9>{
+    -1.0 / 560.0, 8.0 / 315.0, -1.0 / 5.0,  8.0 / 5.0,   -205.0 / 72.0,
+    8.0 / 5.0,    -1.0 / 5.0,  8.0 / 315.0, -1.0 / 560.0};
+
+constexpr auto third_derivative = std::array<double, 9>{
+    -7.0 / 240.0, 3.0 / 10.0,    -169.0 / 120.0, 61.0 / 30.0, 0.0,
+    -61.0 / 30.0, 169.0 / 120.0, -3.0 / 10.0,    7.0 / 240.0};
+
+// for 2-atom molecule
+auto finite_difference(double h = 0.001) {
+  auto density = 0.3;
+
+  Eigen::Vector3d c1(0.0, -3.0 * std::cos(M_PI / 4), -3.0 * std::sin(M_PI / 4));
+  Sphere s1(density, 1.5, c1);
+
+  // build derivative wrt center of s1
+  for (const auto &[order, stencil] : stencils) {
+    SPDLOG_INFO("Computing derivative of order {}", order);
+    for (const auto &[npoints, coefs] : stencil) {
+      SPDLOG_INFO("Using {}-point (centered) stencil", npoints);
+
+      std::vector<Eigen::VectorXd> tmps;
+
+      int i = -(coefs.size() / 2);
+
+      for (const auto c : coefs) {
+        Eigen::Vector3d c0(0.0, 0.0, h * i);
+        Sphere s0(density, 2.0, c0);
+        Eigen::VectorXd ws = TsLess({s0, s1}).weights();
+        SPDLOG_INFO("n_points {}", ws.size());
+
+        tmps.push_back((c / h) * ws);
+
+        i += 1;
+      }
+
+      Eigen::VectorXd dwds1_z =
+          std::accumulate(std::next(tmps.cbegin()), tmps.cend(), tmps[0]);
+
+      SPDLOG_INFO("dwds1_z\n{}", dwds1_z);
+    }
+  }
+
+  // derivative wrt center of s2
+}
+
 int main() {
   spdlog::set_pattern("[%Y-%m-%d %T][%^%l%$][TID: %t, PID: %P][%!@%s:%4#] %v");
 
@@ -75,7 +149,30 @@ int main() {
   auto r2 = 1.5;
   Sphere s2(max_w, r2, c2);
 
+  Eigen::VectorXd Rs(2);
+  Rs << r1, r2;
+  Eigen::Matrix3Xd cs(3, 2);
+  cs.col(0) = c1;
+  cs.col(1) = c2;
+  auto tsk = generate<double>(Rs, cs, max_w);
+
   auto tsless = TsLess({s1, s2});
+
+  SPDLOG_INFO("equal? {}", tsk == tsless.weights());
+
+  // let's try autodiff now
+  VectorXdual Rs_d(2);
+  Rs_d << 1.0, 1.5;
+
+  Vector3dual c1_d(0.0, 0.0, 0.0), c2_d(0.0, 0.0, d);
+  Matrix3Xdual cs_d(3, 2);
+  cs_d.col(0) = c1_d;
+  cs_d.col(1) = c2_d;
+
+  VectorXdual u;
+  Eigen::VectorXd gr1 =
+      gradient(generate<dual>, wrt(cs_d), at(Rs_d, cs_d, max_w), u);
+  // done with autodiff
 
   // compute S, we enforce Hermiticity
   auto S = Smatrix(tsless);
@@ -89,8 +186,8 @@ int main() {
   Eigen::VectorXd q = -(permittivity - 1.0) / (permittivity)*S.ldlt().solve(V);
   // Gauss estimate
   auto gauss = q.sum();
-  SPDLOG_INFO("<< CPCM >>\ntotalASC = {}; gauss = {}; Delta = {}", totalASC, gauss,
-              totalASC - gauss);
+  SPDLOG_INFO("<< CPCM >>\ntotalASC = {}; gauss = {}; Delta = {}", totalASC,
+              gauss, totalASC - gauss);
 
   // isotropic IEFPCM
   auto D = Dmatrix(tsless);
@@ -104,8 +201,35 @@ int main() {
   Eigen::MatrixXd R = (2 * M_PI * Id - D * tsless.weights().asDiagonal());
   q = -T.inverse() * R * V;
   gauss = q.sum();
-  SPDLOG_INFO("<< IEFPCM >>\ntotalASC = {}; gauss = {}; Delta = {}", totalASC, gauss,
-              totalASC - gauss);
+  SPDLOG_INFO("<< IEFPCM >>\ntotalASC = {}; gauss = {}; Delta = {}", totalASC,
+              gauss, totalASC - gauss);
+
+  Vector3dual r_i(0, 0, -1), r_j(0, 0, 1);
+
+  Vector3dual
+      F; // the output scalar u = f(x) evaluated together with gradient below
+
+  Eigen::MatrixXd J = jacobian(
+      f, wrt(r_j), at(r_i, r_j),
+      F); // evaluate the function value u and its gradient vector g = du/dx
+
+  SPDLOG_INFO("F = {}", F);
+  SPDLOG_INFO("J =\n{}", J);
+
+  finite_difference();
+
+  auto density = 0.3;
+  auto gen = [density, c1](double z) {
+    Sphere s1(density, 1.5, c1);
+    Eigen::Vector3d c0(0.0, 0.0, z);
+    Sphere s0(density, 2.0, c0);
+    return TsLess({s0, s1}).weights();
+  };
+
+  auto dwds0_z =
+      utils::FiniteDifference<1, utils::FivePointStencil>::compute(gen);
+
+  SPDLOG_INFO("dwds0_z\n{}", dwds0_z);
 
   return EXIT_SUCCESS;
 }
